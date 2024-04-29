bd72247ede255a43c56e9b35d0390cfa38412b01
kiibra <kibra99@gmail.com>
2024-04-29 18:44:04 -0400

    complete adding bedroom bedNum prop

---
+++ src/Bedroom.jsx
@@ -0,0 +1,12 @@
+// import FloorPlan from "./FloorPlan"
+
+const Bedroom = (props) => {
+  return ( 
+    <div>
+      Bathroom 
+      {props.bedNum}
+    </div>
+  )
+}
+
+export default Bedroom
--- src/FloorPlan.jsx
+++ src/FloorPlan.jsx
@@ -1,12 +1,18 @@
 import Kitchen from "./Kitchen"
 import LivingRoom from "./LivingRoom"
+import Bedroom from "./Bedroom"
+import Bath from "./assets/Bath"
 
 
-const FloorPlan = (props) => {
+const FloorPlan = () => {
   return ( 
     <div>
       < Kitchen />
       < LivingRoom />
+      < Bedroom bedNum={1} />
+      < Bedroom bedNum={2} />
+      < Bedroom bedNum={3} />
+      < Bath />
     </div>
     
   )
